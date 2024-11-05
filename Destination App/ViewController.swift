5bba404cdcc7b1bc50749b67179209f0007e3e26
Septian Nugraha <septiannugraha341@gmail.com>
2024-11-05 19:37:56 +0700

    Initial Commit

---
+++ Destination App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destination App
+//
+//  Created by Muhamad Septian Nugraha on 05/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
